4005462dc959ecfce4c8ccb37049bbf8a7ddf41f
Nicole D'Anton <nicoledanton7@gmail.com>
2025-09-02 20:55:44 -0500

    Finding Companies

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -18,3 +18,41 @@ export const businessList = () => {
     )
     return businessListHTML
 }
+
+
+document
+    .querySelector("#content")
+    .addEventListener(
+            "keypress",
+            (keyPressEvent) => {
+                const companySearchResultArticle = document.querySelector(".foundBusinesses")
+                const businessArray = getBusinesses()
+
+                if (keyPressEvent.charCode === 13) {
+                   const searchTerm = keyPressEvent.target.value
+                   const foundBusiness = businessArray.find(
+                    (business) => business.companyName.toLowerCase().includes(searchTerm.toLowerCase()))
+                        // includes is case senstitive!! 
+                        // this is what it looks like without normalizing the case: 
+                        // const foundBusiness = businessArray.find(
+                            // (business) => business.companyName.includes(searchTerm)
+
+
+                        if (foundBusiness) {
+                            companySearchResultArticle.innerHTML = `
+                                <section class="business">
+                                    <h2 class="business__name">${foundBusiness.companyName}</h2>
+                                    <div class="business__address">
+                                        <p>${foundBusiness.addressFullStreet}</p>
+                                        <p>${foundBusiness.addressCity}, ${foundBusiness.addressStateCode} ${foundBusiness.addressZipCode}</p>
+                                    </div>
+                                    <p><strong>Phone:</strong> ${foundBusiness.phoneWork}</p>
+                                </section>`
+                        } 
+                        else {
+                            // No match found
+                            companySearchResultArticle.innerHTML = `<p>No business found.</p>`
+                        }
+                    }
+                }
+            )
